dd9455e000fc2e990710ee3a0144086d384d5208
jhossepsamuel231 <jhossepsamuel23@gmail.com>
2024-07-02 12:28:03 -0500

    nuevo virtual p causa

--- src/app/service/productos.service.ts
+++ src/app/service/productos.service.ts
@@ -10,7 +10,7 @@ import { Category } from '../models/Categoria.models';
 })
 export class ProductosService {
 
-  url: string = 'http://192.168.101.94:3000/api/v1/';
+  url: string = 'http://192.168.10.116:3000/api/v1/';
   constructor(private http: HttpClient) { }
 
   listarProdcutos() {
